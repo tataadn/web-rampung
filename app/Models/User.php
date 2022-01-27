6fbf5803c44cd105397095cdab9a8e2e117a7b35
Pakhomios Havel. S <if320011@students.del.ac.id>
2022-01-27 22:24:40 +0700

    done

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
 
 class User extends Authenticatable
 {
-    use Notifiable,HasFactory;
     public $timestamps = false;
+    use Notifiable,HasFactory;
     /**
      * The attributes that are mass assignable.
      *
