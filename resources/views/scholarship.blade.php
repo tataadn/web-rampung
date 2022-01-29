aa84861858a127ac9991ce9a28374b8471d51aa3
tataadn <adine.pramesti@gmail.com>
2022-01-29 19:14:29 +0700

    add tampilan baru

---
+++ resources/views/scholarship.blade.php
@@ -0,0 +1,21 @@
+@extends('theme.index')
+
+@section('content')
+
+        <!-- scholarship -->
+        <section id="scholarship">
+            <div class="row m-5 pt-5">
+                <div class="col justify-content-center pt-5">
+                    <h3 class="lh-lg">Apa itu Program Indonesia Paham Statistika? <br>
+                    Mengapa Perlu Ikut? <br>
+                    Jenis kelas yang dibuka pada program Indonesia Paham Statistika? <br>
+                    Apa saja yang dipelajari di program Indonesia Paham Statistika? <br>
+                    Siapa yang bisa ikutan program ini? <br>
+                    Bagaimana cara ikutan program ini? <br>
+                    Mekanisme belajar</h3>
+                </div>
+            </div>
+        </section>
+        <!-- scholarship -->
+
+@endsection
---
+++ resources/views/solution.blade.php
@@ -0,0 +1,238 @@
+@extends('theme.index')
+
+@section('content')
+
+        <!-- jumbotron -->
+        <section class="content">
+            <div class="container mb-4">
+                <div class="row" style="padding: 7rem 2rem 3rem 2rem;">
+                    <div class="col-md-6 mb-4" data-aos="fade-up" data-aos-duration="2000">
+                        <h1 class="mb-3 display-4 fw-bold"><a href="" style="text-decoration: none; cursor: text; color: orange;">Solusi</a> Kebutuhan</h1>
+                        <h1 class="mb-5 display-4 fw-bold">Penelitianmu!</h1>
+                        <h6 class="mb-5 text-justify lh-base" style="text-align: justify;">Kami hadir dengan menyediakan servis olah data sesuai dengan kebutuhan Anda. Kami menggunakan bantuan Machine Learning dan Artificial Intelegence (AI) untuk memaksimalkan pengolahan data sehingga menghasilkan output yang sesuai dengan kebutuhan Anda.
+                        </h6>
+                        <a class="btn btn-warning btn-rounded text-uppercase" style="color: black;" href="" role="button"><h6>tertarik? mari berdiskusi lebih lanjut</h6></a>
+                    </div>
+                    <div class="col-md-6 justify-content-center" data-aos="zoom-in" data-aos-duration="2000">
+                        <img src="img/pic.png" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- jumbotron -->
+
+        <!-- rala report -->
+        <section id="rala-report">
+            <div class="container p-3">
+            <div class="row">
+                <div class="col mb-3">
+                <h2 class="fw-bold">More Report on Rala Report</h2>
+                </div>
+            </div>
+            <div class="row m-3 justify-content-center align-items-center">
+                <div class="col-sm-4 pt-3">
+                <div class="mb-3" style="max-width: 400px;">
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img
+                        src="https://novapublishers.com/wp-content/uploads/2020/09/9781536187502-1000x1623.jpg"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                        />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Education <br> Report by RALA Report
+                        </h5>
+                        <p class="card-text">
+                            <a href="">Free</a>
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-2 justify-content-center text-center">
+                        <a href="" class="btn btn-warning text-uppercase mt-2 btn-block" style="color: black;">download</a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-sm-4 pt-3">
+                <div class="mb-3" style="max-width: 450px;">
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img
+                        src="https://novapublishers.com/wp-content/uploads/2020/09/9781536187502-1000x1623.jpg"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                        />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Education <br> Report by RALA Report
+                        </h5>
+                        <p class="card-text">
+                            <a href="">Free</a>
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-2 justify-content-center text-center">
+                        <a href="" class="btn btn-warning text-uppercase mt-2 btn-block" style="color: black;">download</a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-sm-4 pt-3">
+                <div class="mb-3" style="max-width: 450px;">
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img
+                        src="https://novapublishers.com/wp-content/uploads/2020/09/9781536187502-1000x1623.jpg"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                        />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Education <br> Report by RALA Report
+                        </h5>
+                        <p class="card-text">
+                            <a href="">Berbayar</a>
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-2 justify-content-center text-center">
+                        <a href="" class="btn btn-warning text-uppercase mt-2 btn-block" style="color: black;">download</a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="row m-3 justify-content-center align-items-center">
+                <div class="col-sm-4 pt-3">
+                <div class="mb-3" style="max-width: 450px;">
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img
+                        src="https://novapublishers.com/wp-content/uploads/2020/09/9781536187502-1000x1623.jpg"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                        />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Education <br> Report by RALA Report
+                        </h5>
+                        <p class="card-text">
+                            <a href="">Free</a>
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-2 justify-content-center text-center">
+                        <a href="" class="btn btn-warning text-uppercase mt-2 btn-block" style="color: black;">download</a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-sm-4 pt-3">
+                <div class="mb-3" style="max-width: 450px;">
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img
+                        src="https://novapublishers.com/wp-content/uploads/2020/09/9781536187502-1000x1623.jpg"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                        />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Education <br> Report by RALA Report
+                        </h5>
+                        <p class="card-text">
+                            <a href="">Free</a>
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-2 justify-content-center text-center">
+                        <a href="" class="btn btn-warning text-uppercase mt-2 btn-block" style="color: black;">download</a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-sm-4 pt-3">
+                <div class="mb-3" style="max-width: 450px;">
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img
+                        src="https://novapublishers.com/wp-content/uploads/2020/09/9781536187502-1000x1623.jpg"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                        />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Education <br> Report by RALA Report
+                        </h5>
+                        <p class="card-text">
+                            <a href="">Rp 50.000</a>
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt-2 justify-content-center text-center">
+                        <a href="" class="btn btn-warning text-uppercase mt-2 btn-block" style="color: black;">download</a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+        <!-- rala report -->
+
+        <!-- report bayar -->
+        <section id="report-bayar">
+            <div class="container p-3">
+            <div class="row m-3">
+                <div class="col-md-3">
+                <div class="card">
+                    <img src="https://novapublishers.com/wp-content/uploads/2020/09/9781536187502-1000x1623.jpg" class="card-img-top" alt="Fissure in Sandstone" style="max-width: 450px;"/>
+                    <div class="card-body">
+                    <h6 class="card-title">Education Report by RALA Report</h6>
+                    <a href="">Rp 50.000</a>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-7">
+                <div class="card">
+                    <div class="card-body">
+                    <h3 class="card-title mb-4">REPORT - Penggunaan Big Data Analytic dan Artificial Intelligence Sebagai Inovasi Pendidikan di Era Digital</h3>
+                    <p style="text-align: justify;" class="lh-baseline">Revolusi Industri 4.0 (4.0 IR) ditandai dengan penerapan inovasi manufacturing proces berupa otomisasi dan custom mass production dengan memanfaatkan Big Data, Internet Of Thing, Artificial Inteligent. Demikian juga Tata Kelola Manajemen Aset Barang Milik Negara mengikuti era otomisasi dan kustomisasi agar tetap sustainable. Oleh sebab itu, perlu pemahaman awal definisi dan penerapan Big Data, Internet Of Thing, dan Artificial Inteligent.
+                    </p>
+                    <div class="row justify-content-center text-center">
+                        <div class="col-md-4">
+                        <a href="" class="btn btn-outline-primary text-uppercase fw-bold" style="color: black;"><h2 class="m-2">bayar</h2></a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-2">
+                <div class="border border-dark" style="text-align: center;">
+                    <div class="card-body m-5">
+                    <h5 class="card-title">S</h5>
+                    <h5 class="card-title">L</h5>
+                    <h5 class="card-title">O</h5>
+                    <h5 class="card-title">T</h5>
+                    <br>
+                    <h5 class="card-title">I</h5>
+                    <h5 class="card-title">K</h5>
+                    <h5 class="card-title">L</h5>
+                    <h5 class="card-title">A</h5>
+                    <h5 class="card-title">N</h5>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+        <!-- report bayar -->
+
+@endsection
